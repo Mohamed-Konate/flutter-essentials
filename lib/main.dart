a44c837e333967684c4c4ddff98ae55ad6c605a6
Mohamed Konaté <m.konate@mdcmali.com>
2020-10-24 11:16:14 +0000

    Container element

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,7 @@ class FlutterEssentials extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Essentials',
       home: HomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
@@ -22,8 +20,29 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home page'),
       ),
-      body: Container(
-        child: Center(child: Text('Welcome to my home page')),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [Colors.pink, Colors.yellow]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+                ),
+              ]),
+          child: Text(
+            'I am a Box',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
